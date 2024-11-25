cc6d29f7e86152859d67ab82cdc718799b13f836
relmer <relmer@hotmail.com>
2024-11-25 13:06:11 -0800

    feat(cpu): initial Group 01 instruction scaffolding

--- My6502/Cpu.cpp
+++ My6502/Cpu.cpp
@@ -1,4 +1,8 @@
+#include "Pch.h"
+
 #include "Cpu.h"
+
+
 
 void Cpu::Reset ()
 {
@@ -6,4 +10,36 @@ void Cpu::Reset ()
     sp = 0x0100;
 
     status.status = 0;
+
+    a = 0;
+    x = 0;
+    y = 0;
+
+    memset (memory, 0, sizeof (memory));
+}
+
+
+
+void Cpu::Run ()
+{
+    do
+    {
+        byte opcode         = memory[pc];
+        byte instruction    = Decode (opcode); 
+    }
+    while (0);
+}
+
+
+
+word Cpu::Decode (byte opcode)
+{
+    return word ();
+}
+
+
+
+byte Cpu::FetchInstruction ()
+{
+    return byte ();
 }
--- My6502/Cpu.h
+++ My6502/Cpu.h
@@ -27,15 +27,65 @@ union Status
 
 
 
+
+enum Opcode01
+{
+    ORA,    // 000 = OR
+    AND,    // 001 = AND
+    EOR,    // 010 = XOR
+    ADC,    // 011 = ADD with Carry
+    STA,    // 100 = Store
+    LDA,    // 101 = Load
+    CMP,    // 110 = Compare
+    SBC     // 111 = Subtract with Carry
+};
+
+
+
+enum AddressingMode01
+{
+    ZeroPageXIndirect,  // 000 (zero page, X)
+    ZeroPage,           // 001 zero page
+    Immediate,          // 010 #immediate
+    Absolute,           // 011 absolute
+    ZeroPageIndirectY,  // 100 (zero page), Y
+    ZeroPageX,          // 101 zero page, X
+    AbsoluteY,          // 110 absolute, Y
+    AbsoluteX           // 111 absolute, X
+};
+
+
+
+struct Instruction
+{
+    byte Opcode         : 3;
+    byte AddressingMode : 3;
+    byte Group          : 2;
+};
+
+
+
 class Cpu
 {
 public:
     void Reset ();
+    void Run ();
 
 protected:
+    word Decode (byte opcode);
+    byte FetchInstruction ();
+
+protected:
+    static size_t constexpr     memSize = 64 * 1024;
+    byte                        memory[memSize];
+
     word    pc;
     word    sp;
 
     Status  status;
+
+    byte    a;
+    byte    x;
+    byte    y;
 };
 
