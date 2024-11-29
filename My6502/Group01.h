6e6226711d2e5a45661de5ef53719cc76520af48
relmer <relmer@hotmail.com>
2024-11-29 15:18:59 -0800

    refactor(core): extract Group01, Instruction, Microcode to separate headers

---
+++ My6502/Group01.h
@@ -0,0 +1,79 @@
+#pragma once
+
+
+
+class Group01
+{
+protected:
+    static constexpr Byte group = 0b01;
+
+public:
+    enum Opcode
+    {
+        ORA,    // 000 = OR
+        AND,    // 001 = AND
+        EOR,    // 010 = XOR
+        ADC,    // 011 = ADD with Carry
+        STA,    // 100 = Store
+        LDA,    // 101 = Load
+        CMP,    // 110 = Compare
+        SBC     // 111 = Subtract with Carry
+    };
+
+    static constexpr const char * instructionName[] =
+    {
+        "ORA",
+        "AND",
+        "EOR",
+        "ADC",
+        "STA",
+        "LDA",
+        "CMP",
+        "SBC"
+    };
+
+    enum AddressingMode
+    {
+        __AM_First = 0,
+        AM_ZeroPageXIndirect = 0b000,   // 000 (Zero Page, X) -> ($00LL + X)
+        AM_ZeroPage = 0b001,   // 001 Zero Page
+        AM_Immediate = 0b010,   // 010 #Immediate
+        AM_Absolute = 0b011,   // 011 Absolute
+        AM_ZeroPageIndirectY = 0b100,   // 100 (Zero Page), Y
+        AM_ZeroPageX = 0b101,   // 101 Zero Page, X
+        AM_AbsoluteY = 0b110,   // 110 Absolute, Y
+        AM_AbsoluteX = 0b111,   // 111 Absolute, X
+        __AM_Count
+    };
+
+    enum AddressingModeFlag
+    {
+        AMF_ZeroPageXIndirect = 0b00000001,
+        AMF_ZeroPage = 0b00000010,
+        AMF_Immediate = 0b00000100,
+        AMF_Absolute = 0b00001000,
+        AMF_ZeroPageIndirectY = 0b00010000,
+        AMF_ZeroPageX = 0b00100000,
+        AMF_AbsoluteY = 0b01000000,
+        AMF_AbsoluteX = 0b10000000,
+        __AMF_AllModes = 0b11111111
+    };
+
+    static constexpr const char * addressingModeName[] =
+    {
+        "(Zero Page, X)",
+        "Zero Page",
+        "#Immediate",
+        "Absolute",
+        "(Zero Page), Y",
+        "Zero Page, X",
+        "Absolute, Y",
+        "Absolute, X"
+    };
+
+public:
+    static Instruction CreateInstruction (Opcode opcode, AddressingMode addressingMode)
+    {
+        return Instruction (opcode, addressingMode, group);
+    }
+};
